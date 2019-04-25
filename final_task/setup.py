e1c38fed81072c7d2240f94e98abf34f888e4b4a
Pavel Kuzmich <Pavel_Kuzmich@epam.com>
2019-04-25 14:12:05 +0300

    started working on unit testing

--- final_task/setup.py
+++ final_task/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycalc',
-    version='1.0.1',
+    version='1.2.0',
     author='Pavel Kuzmich',
-    author_email='pavelkuz99@outlook.com',
+    author_email='Pavel_Kuzmich@epam.com',
     description='Pure Python command-line calculator',
     packages=find_packages(),
     entry_points={
